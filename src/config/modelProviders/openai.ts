0a0a0fcbf0da6cf61070f506ed219c5c8e21ec39
flootof <19514895+fleetof@users.noreply.github.com>
2025-04-21 11:33:37 +0800

    修改openai仓库

--- src/config/modelProviders/openai.ts
+++ src/config/modelProviders/openai.ts
@@ -25,7 +25,7 @@ const OpenAI: ModelProviderCard = {
       description:
         'o1-mini是一款针对编程、数学和科学应用场景而设计的快速、经济高效的推理模型。该模型具有128K上下文和2023年10月的知识截止日期。',
       displayName: 'OpenAI o1-mini',
-      enabled: true,
+      enabled: false,
       id: 'o1-mini',
       maxOutput: 65_536,
       pricing: {
@@ -39,7 +39,7 @@ const OpenAI: ModelProviderCard = {
       description:
         'o1是OpenAI新的推理模型，支持图文输入并输出文本，适用于需要广泛通用知识的复杂任务。该模型具有200K上下文和2023年10月的知识截止日期。',
       displayName: 'OpenAI o1',
-      enabled: true,
+      enabled: false,
       id: 'o1-2024-12-17',
       maxOutput: 100_000,
       pricing: {
@@ -54,7 +54,7 @@ const OpenAI: ModelProviderCard = {
       description:
         'o1是OpenAI新的推理模型，适用于需要广泛通用知识的复杂任务。该模型具有128K上下文和2023年10月的知识截止日期。',
       displayName: 'OpenAI o1-preview',
-      enabled: true,
+      enabled: false,
       id: 'o1-preview',
       maxOutput: 32_768,
       pricing: {
@@ -68,7 +68,7 @@ const OpenAI: ModelProviderCard = {
       description:
         'GPT-4o mini是OpenAI在GPT-4 Omni之后推出的最新模型，支持图文输入并输出文本。作为他们最先进的小型模型，它比其他近期的前沿模型便宜很多，并且比GPT-3.5 Turbo便宜超过60%。它保持了最先进的智能，同时具有显著的性价比。GPT-4o mini在MMLU测试中获得了 82% 的得分，目前在聊天偏好上排名高于 GPT-4。',
       displayName: 'GPT-4o mini',
-      enabled: true,
+      enabled: false,
       functionCall: true,
       id: 'gpt-4o-mini',
       maxOutput: 16_385,
@@ -83,7 +83,7 @@ const OpenAI: ModelProviderCard = {
       description:
         'ChatGPT-4o 是一款动态模型，实时更新以保持当前最新版本。它结合了强大的语言理解与生成能力，适合于大规模应用场景，包括客户服务、教育和技术支持。',
       displayName: 'GPT-4o 1120',
-      enabled: true,
+      enabled: false,
       functionCall: true,
       id: 'gpt-4o-2024-11-20',
       pricing: {
@@ -98,7 +98,7 @@ const OpenAI: ModelProviderCard = {
       description:
         'ChatGPT-4o 是一款动态模型，实时更新以保持当前最新版本。它结合了强大的语言理解与生成能力，适合于大规模应用场景，包括客户服务、教育和技术支持。',
       displayName: 'GPT-4o',
-      enabled: true,
+      enabled: false,
       functionCall: true,
       id: 'gpt-4o',
       pricing: {
@@ -140,7 +140,7 @@ const OpenAI: ModelProviderCard = {
       description:
         'ChatGPT-4o 是一款动态模型，实时更新以保持当前最新版本。它结合了强大的语言理解与生成能力，适合于大规模应用场景，包括客户服务、教育和技术支持。',
       displayName: 'ChatGPT-4o',
-      enabled: true,
+      enabled: false,
       id: 'chatgpt-4o-latest',
       pricing: {
         input: 5,
