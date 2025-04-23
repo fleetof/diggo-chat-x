35b189e8c8753c504b24a842b711c0f1d47e9288
flootof <2932951035@qq.com>
2025-04-23 16:33:55 +0800

    添加启动页logo

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -1,19 +1,7 @@
-/*
- * @Author: flootof 2932951035@qq.com
- * @Date: 2025-04-17 17:25:52
- * @LastEditors: flootof 2932951035@qq.com
- * @LastEditTime: 2025-04-21 10:46:17
- * @FilePath: /diggo-chat-fork/src/const/branding.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-// the code below can only be modified with commercial license
-// if you want to use it in the commercial usage
-// please contact us for more information: hello@lobehub.com
-
 export const LOBE_CHAT_CLOUD = 'Diggo Chat Cloud';
 
 export const BRANDING_NAME = 'Diggo Chat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_LOGO_URL = '/icons/icon-192x192.png';
 
 export const ORG_NAME = 'Diggo Chat';
 
